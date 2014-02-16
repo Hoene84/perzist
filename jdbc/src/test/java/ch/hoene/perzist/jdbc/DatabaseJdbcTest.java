61d7b068d90fbec68a569aaa45d187d87f4d7466
Simon Honegger <simon.honegger@gmail.com>
2014-02-16 19:58:04 +0100

    init junit mvn dependency

---
+++ jdbc/src/test/java/ch/hoene/perzist/jdbc/DatabaseJdbcTest.java
@@ -0,0 +1,42 @@
+package ch.hoene.perzist.jdbc;
+
+import ch.hoene.perzist.access.db.Database;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class DatabaseJdbcTest {
+
+    Database database = new DatabaseJdbc("");
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testCount() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testGetList() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testPush() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testDrop() throws Exception {
+        assertTrue(false);
+    }
+}
